858412fd694cb769fb427e3323d2e1344ff10d98
Protesilaos Stavrou <info@protesilaos.com>
2025-05-22 08:45:55 +0300

    Bump version to trigger GNU ELPA package build

--- doric-themes.el
+++ doric-themes.el
@@ -5,7 +5,7 @@
 ;; Author: Protesilaos Stavrou <info@protesilaos.com>
 ;; Maintainer: Protesilaos Stavrou <info@protesilaos.com>
 ;; URL: https://github.com/protesilaos/doric-themes
-;; Version: 0.0.0
+;; Version: 0.0.1
 ;; Package-Requires: ((emacs "29.1"))
 ;; Keywords: faces, theme, accessibility
 
